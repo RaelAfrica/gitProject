669c52f9f3aebff0f80f7b21bfa32ea02191afba
Rael Kenny <RaelKenny@gmail.com>
2017-02-17 13:27:13 -0500

    initial commit

---
+++ gitProject/gitProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitProject
+//
+//  Created by Rael Kenny on 2/17/17.
+//  Copyright © 2017 Rael Kenny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
